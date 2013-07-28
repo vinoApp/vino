fe4b0eeba033cb452616abc33c6bd629249c7261
walien <eoriou@gmail.com>
2013-07-28 14:09:21 -0400

    [mobile] - Add REST client (Volley) for bottle retrieving - The barcode is now successfully sent to the server

--- vino-mobile/src/com/vino/mobile/VinoMainActivity.java
+++ vino-mobile/src/com/vino/mobile/VinoMainActivity.java
@@ -7,29 +7,28 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import com.android.volley.Response;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class VinoMainActivity extends Activity implements View.OnClickListener {
 
     private Button scanBtn;
-    private TextView formatTxt, contentTxt;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        scanBtn = (Button) findViewById(R.id.scanButton);
-        formatTxt = (TextView) findViewById(R.id.scanFormat);
-        contentTxt = (TextView) findViewById(R.id.scanContent);
-
+        scanBtn = (Button) findViewById(R.id.scanBtn);
         scanBtn.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        if (view.getId() == R.id.scanButton) {
+        if (view.getId() == R.id.scanBtn) {
             IntentIntegrator scanIntegrator = new IntentIntegrator(this);
             scanIntegrator.initiateScan();
         }
@@ -40,11 +39,27 @@ public class VinoMainActivity extends Activity implements View.OnClickListener {
 
         IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
         if (scanningResult != null) {
-            formatTxt.setText("FORMAT: " + scanningResult.getFormatName());
-            contentTxt.setText("CONTENT: " + scanningResult.getContents());
+            WineBottleRESTUnit.retrieveWineBottle(VinoMainActivity.this, scanningResult.getContents(),
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject jsonObject) {
+                            // Update UI about domain, vintage & sticker image
+                            try {
+                                ((TextView) findViewById(R.id.domainText)).setText(jsonObject.getJSONObject("domain").getString("name"));
+                                ((TextView) findViewById(R.id.vintageText)).setText(jsonObject.getString("vintage"));
+                                //activity.findViewById(R.id.)
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
         } else {
             Toast toast = Toast.makeText(getApplicationContext(), "No scan data received!", Toast.LENGTH_SHORT);
             toast.show();
         }
     }
+
+
 }
