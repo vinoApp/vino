40c15a7ba0944fc824a1c1cabbd4ed6ce078c17f
Elian ORIOU <elian.oriou@4sh.fr>
2014-02-10 00:16:36 +0100

    Improve domains list

--- vino-web/app/scripts/controllers/domain/DomainsCtrl.js
+++ vino-web/app/scripts/controllers/domain/DomainsCtrl.js
@@ -1,5 +1,20 @@
-angular.module('vino.ui').controller("DomainsCtrl", function ($scope, Domain) {
+angular.module('vino.ui').controller("DomainsCtrl", function ($scope, Domain, Notification) {
 
     $scope.domains = Domain.query();
+
+    $scope.remove = function (domain) {
+
+        Notification.dialog.confirm('Would you really want to remove this domain ?', null,
+            function (yes) {
+                if (yes) {
+                    Domain.remove({id: domain._id}, function () {
+                        Notification.notify.success("Domain successfuly removed.");
+                        $scope.domains = Domain.query();
+                    }, function () {
+                        Notification.notify.error("Error during deletion.");
+                    });
+                }
+            });
+    };
 
 });
