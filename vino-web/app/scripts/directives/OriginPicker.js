6b0d95d96ceab4a8f52118f16e061ecd08285f3f
Elian ORIOU <elian.oriou@4sh.fr>
2014-03-29 20:58:35 +0100

    Improve OriginPicker component (with select2 and optGroup)

--- vino-web/app/scripts/directives/OriginPicker.js
+++ vino-web/app/scripts/directives/OriginPicker.js
@@ -1,33 +1,92 @@
-angular.module("vino.ui").directive("originPicker", function () {
+angular.module("vino.ui").directive("originPicker", function (Origin) {
 
     return {
         restrict: 'E',
         scope: {
             ngModel: '=',
-            ngRequired: '='
+            ngRequired: '=',
+            ngDisabled: '='
         },
-        template: '' +
-            '<select id="domainOrigin" ng-required="ngRequired" ng-model="ngModel" ng-options="aoc.name group by aoc.region.name for aoc in aocs">' +
-            '</select>',
-        controller: function ($scope, Origin) {
+        link: function ($scope, elt) {
 
-            $scope.$watchCollection("aocs", function () {
-                if (!$scope.aocs) {
+            var loadData = function (aocs) {
+                $scope.data = {};
+                _.each(aocs, function (aoc) {
+                    var _regionId = aoc.region._id;
+                    var _region = $scope.data[_regionId];
+                    if (!_region) {
+                        $scope.data[_regionId] = {
+                            id: _regionId,
+                            name: aoc.region.name,
+                            children: []
+                        };
+                    }
+                    var region = $scope.data[_regionId];
+                    region.children.push(aoc);
+                });
+                $scope.data = _.values($scope.data);
+            };
+
+            var format = function (item) {
+                return item.name;
+            };
+
+            var findIndex = function (value) {
+                return _.findIndex($scope.aocs, function (aoc) {
+                    if (_.isObject(value)) {
+                        return aoc._id === value._id;
+                    }
+                    return aoc._id === value;
+                });
+            };
+
+            var element = $(elt);
+
+            function render() {
+
+                element.select2({
+                    data: {
+                        results: $scope.data,
+                        text: 'name'
+                    },
+                    id: function (item) {
+                        return item._id;
+                    },
+                    formatSelection: format,
+                    formatResult: format,
+                    initSelection: function (element, callback) {
+                        if (!$scope.ngModel) {
+                            return;
+                        }
+                        var aoc = $scope.aocs[findIndex($scope.ngModel)];
+                        callback(aoc);
+                    },
+                    width: '100%'
+                });
+            }
+
+            $scope.$watchCollection("[aocs.length, ngModel]", function () {
+
+                if (!$scope.aocs || $scope.aocs.length < 1) {
                     return;
                 }
-                $scope.$watch("ngModel", function () {
-                    if (!$scope.ngModel) {
-                        return;
-                    }
-                    var foundAOC = _.find($scope.aocs, {_id: $scope.ngModel._id});
-                    if (foundAOC) {
-                        $scope.ngModel = foundAOC;
-                    }
+                render();
+            });
+
+            element.change(function () {
+                $scope.$apply(function () {
+                    $scope.ngModel = $scope.aocs[findIndex(element.val())];
                 });
             });
 
-            $scope.aocs = Origin.getAllAOCS();
+            $scope.$watch("ngDisabled", function () {
+                element.prop('disabled', $scope.ngDisabled);
+                render();
+            });
 
+            $scope.aocs = Origin.getAllAOCS(function (aocs) {
+                loadData(aocs);
+            });
         }
     }
 
